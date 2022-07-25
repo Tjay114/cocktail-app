b229a00d8a6df3984c6278a4e0ce8017da1ada35
Terormumuli <terahmumuli@gmail.com>
2022-07-25 13:16:34 +0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <div className="card">
+            <input type="text" placeholder="Search Cocktail" />
+        </div>
+    )
+}
+
+export default NavBar;
